ca39e35ef1eecb8fcfe7c55a11b65cd580821b7e
Evan Berg <evan.m.berg@gmail.com>
2022-09-23 16:09:55 -0400

    beginning stages of building new bet forms

--- src/components/NewBetContainer.js
+++ src/components/NewBetContainer.js
@@ -5,30 +5,55 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 axios.defaults.withCredentials = true;
 const API_URL = AuthServices.getBaseUrl();
 
 const NewBet = () => {
+  const initialFormState = {
+    user_id: null,
+    main_cause_id: null,
+    sub_cause_id: null,
+    timeframe: '',
+    amount: null
+  }
   const user = AuthServices.getCurrentUser();
-  const [mainCauses, setMainCauses] = useState([]);
-  const [subCauses, setSubCauses] = useState([]);
-  const [newBet, setNewBet] = useState([]);
+  const [currentCauses, setCurrentCauses] = useState([]);
+  const [newBet, setNewBet] = useState(initialFormState);
 
   useEffect(() => {
+    setNewBet({...newBet, ['user_id']: user.id});
     axios.get(API_URL+'bets/all_causes_for_dropdown').then(resp=>{
-      console.log(resp);
-      let main_causes = resp.data.main_causes;
-      let sub_causes = resp.data.sub_causes;
-      setMainCauses(main_causes);
-      setSubCauses(sub_causes);
+      let causes = resp.data.causes;
+      setCurrentCauses(causes);
     });
   }, []);
+
+  const handleInputChange = (event) => {
+    const {name, value} = event.target;
+    setNewBet({...newBet, [name]: value});
+  }
 
   return(
     <div>
       <Typography variant='h4'>Make a New Bet!</Typography>
       <div className='newBet'>
-
+        <InputLabel>Main Cause</InputLabel>
+        <Select 
+          label='mainCause'
+          name='main_cause_id'
+          onChange={handleInputChange}
+        >
+          {currentCauses.map((cause, index) => 
+            <MenuItem value={cause.id} key={cause.id}>{cause.title}</MenuItem>
+          )}
+        </Select>
+        <InputLabel>Sub Cause</InputLabel>
+        <Select label='subCause'>
+            
+        </Select>
       </div>
     </div>
   )
