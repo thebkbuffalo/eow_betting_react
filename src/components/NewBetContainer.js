1cd0066556bcf60a0aa1ee1b09fd9abff8f18088
Evan Berg <evan.m.berg@gmail.com>
2022-10-17 17:49:50 -0400

    starting to set up new categories creation form and such

--- src/components/NewBetContainer.js
+++ src/components/NewBetContainer.js
@@ -8,11 +8,25 @@ import Button from '@mui/material/Button';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import Modal from '@mui/material/Modal';
 import { Input, Menu } from '@mui/material';
 axios.defaults.withCredentials = true;
 const API_URL = AuthServices.getBaseUrl();
 const numbersArrayYears = Array.from(Array(100).keys());
-const numbersArrayMonths = Array.from(Array(12).keys())
+const numbersArrayMonths = Array.from(Array(12).keys());
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '60%',
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  padding: '15px',
+  boxShadow: 24
+};
+
 
 // const dayMonthYear = ['Days', 'Months', 'Years']
 
@@ -29,6 +43,10 @@ const NewBet = () => {
   const [currentCauses, setCurrentCauses] = useState([]);
   const [subCauses, setSubCauses] = useState([]);
   const [newBet, setNewBet] = useState(initialFormState);
+  const [selectedCauseName, setSelectedCauseName] = useState('');
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     setNewBet({...newBet, ['user_id']: user.id});
@@ -45,6 +63,7 @@ const NewBet = () => {
       setNewBet({...newBet, [name]: value.id});
       if(name === 'main_cause_id'){
         setSubCauses(value.subs);
+        setSelectedCauseName(value.title);
       }
     }
   }
@@ -112,24 +131,30 @@ const NewBet = () => {
           variant='contained'
           color='primary' 
           className='saveBet'
-          onClick={createNewBet}>Save
+          onClick={createNewBet}>
+            Save
         </Button>
-        {/* <div className='timeframe'>
-          <Typography varian='h5'>Timeframe</Typography>
-          <InputLabel>Number of:</InputLabel>
-          <Select label='timeframe' name='timeframe' defaultValue=''>
-            {numbersArray.map((num) =>
-              <MenuItem value={num} key={num}>{num}</MenuItem>
-            )}
-          </Select>
-          <InputLabel>Days/Months/Years</InputLabel>
-          <Select label='timeframe' name='timeframe' defaultValue=''>
-            {dayMonthYear.map((dmy) =>
-              <MenuItem value={dmy} key={dmy}>{dmy}</MenuItem>
-            )}
-          </Select>
-        </div> */}
       </div>
+      <div className='newCausesTxt'>
+        <Typography variant='h5'>Not seeing a cause or subcause you think will win? Worry not! You can always edit add subcauses to an existing cause or add an entirely new cause!</Typography>
+        <Button 
+          variant='contained'
+          color='secondary' 
+          className='newCause'
+          onClick={handleOpen}>
+            Add/Edit Cause
+        </Button>
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography variant='h3'>Add some new shit {newBet.main_cause_id}</Typography>
+        </Box>
+      </Modal>
     </div>
   )
 }
